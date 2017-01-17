dd37b44519bb63af58013b27f3f3d6f2acb544b0
Justin179 <Justin179>
2017-01-17 14:17:56 +0800

    function expression 內是獨立的scope

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
-// function constructor
-function Person(firstname, lastname){
-    this.firstname = firstname;
-    this.lastname = lastname;
-}
+var firstname = 'jane';
 
-// 在該物件的prototype放進一個greet方法
-Person.prototype.greet = function(){
-    console.log('hello, '+this.firstname+' '+this.lastname);
-}
+// function expression(在這裡面有獨立的scope)
+(function(){
+    var firstname = 'john';
+    console.log(firstname);
+})();
 
-// 物件實體化
-var person = new Person('John','Doe');
-// person本身並沒有greet這個方法，這是繼承而來的方法
-person.greet();
-
-// 另一個物件也share了同樣的方法
-var person2 = new Person('jane','doe');
-person2.greet(); 
-
-// 證明是share同一個物件
-console.log(person.__proto__ === person2.__proto__);
+console.log(firstname);

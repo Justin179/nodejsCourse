c22a4613efe2ee46e19cacd3dcd1857940d55333
Justin179 <Justin179>
2017-01-17 17:20:07 +0800

    loop through an array

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-const util = require('util');
+// object properties and methods
+var obj = {
+    greet: 'hello'
+}
 
-var name = 'Justin';
-var greeting = util.format('Hello, %s',name);
-util.log(greeting);
+console.log(obj.greet);
+console.log(obj['greet']);
+var prop = 'greet';
+console.log(obj[prop]);
+
+var arr = [];
+arr.push(function(){
+    console.log('hi 1');
+});
+arr.push(function(){
+    console.log('hi 2');
+});
+arr.push(function(){
+    console.log('hi 3');
+});
+
+// item就是陣列中的每一個物件
+arr.forEach(function(item){
+    item();
+});

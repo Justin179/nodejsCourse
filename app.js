eabc470164aceb9b2dddddceb966a02bc0b71af2
Justin179 <Justin179>
2017-01-17 13:46:05 +0800

    javascript 不同物件share同一個prototype(繼承)

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-var person = {
-    firstname: 'John',
-    lastname: 'Doe',
-    greet: function(){
-        console.log('Hello, '+this.firstname+" "+this.lastname);
-    }
-};
+// function constructor
+function Person(firstname, lastname){
+    this.firstname = firstname;
+    this.lastname = lastname;
+}
 
+// 在該物件的prototype放進一個greet方法
+Person.prototype.greet = function(){
+    console.log('hello, '+this.firstname+' '+this.lastname);
+}
+
+// 物件實體化
+var person = new Person('John','Doe');
+// person本身並沒有greet這個方法，這是繼承而來的方法
 person.greet();
 
-console.log(person['firstname']);
+// 另一個物件也share了同樣的方法
+var person2 = new Person('jane','doe');
+person2.greet(); 
+
+// 證明是share同一個物件
+console.log(person.__proto__ === person2.__proto__);

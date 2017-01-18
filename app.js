5437fedbf471bb45c1a03a5aba98fce35d2c9635
Justin179 <Justin179>
2017-01-18 16:45:35 +0800

    把txt的文字，完全複製到另一個txt檔案

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var fs = require('fs');
-var readable = fs.createReadStream(__dirname+'/greet.txt',{encoding:'utf-8'});
+var readable = fs.createReadStream(__dirname+'/greet.txt'
+,{encoding:'utf-8',highWaterMark:32 * 1024});
+// highWaterMark: each chunk's size
+
+var writable = fs.createWriteStream(__dirname+'/greet2.txt');
 
 readable.on('data',function(chunk){
     console.log(chunk);
+    writable.write(chunk); // 把資料寫(複製)到greet2.txt
 });

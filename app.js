afcec2cb8fba3b045ce4ccedd15529ce3e67df42
Justin179 <Justin179>
2017-01-18 19:20:58 +0800

    first nodejs web server

--- app.js
+++ app.js
@@ -1,27 +1,12 @@
-var fs = require('fs');
-var zlib = require('zlib');
+// the callback is an event listener
 
-// read from greet.txt
-var readable = fs.createReadStream(__dirname+'/greet.txt'
-,{encoding:'utf-8',highWaterMark:32 * 1024});
-// highWaterMark: each chunk's size
+var http = require('http');
 
-// write to greet2.txt
-var writable = fs.createWriteStream(__dirname+'/greet2.txt');
+http.createServer(function(req,res){
 
-// when read a chunk of data from readable, invoke the function
-// readable.on('data',function(chunk){
-//     console.log(chunk);
-//     writable.write(chunk); // 把資料寫(複製)到greet2.txt
-// });
+    // 組response
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.end("Hello World!!\n");
 
-// 用pipe簡化寫法, 從file讀進來，(通過pipe)，寫出去到file
-
-var compressed = fs.createWriteStream(__dirname+'/greet.txt.gz');
-
-var gzip = zlib.createGzip();
-
-readable.pipe(writable);
-
-// on every chunk pipe to gzip, then piping the compressed data to a file
-readable.pipe(gzip).pipe(compressed);
+}).listen(1337,'127.0.0.1');
+// localhost:1337 這個program就會收到

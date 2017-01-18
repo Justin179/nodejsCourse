0df62faf02d47b58c6108311211818044f2bb8e5
Justin179 <Justin179>
2017-01-18 12:02:27 +0800

    prototype chain 重要觀念

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
-// call and apply
-var obj = {
-    name: 'Justin Chen',
-    greet: function(param){
-        console.log(`Hello ${this.name}`);
-    }
+// require 'events' & 'util'
+var EventEmitter = require('events');
+var util = require('util');
+
+// create function constructor Greetr
+function Greetr(){
+    EventEmitter.call(this);
+    // this keyword refers to every new obj being created
+    this.greeting = "Hello world";
 }
 
-obj.greet();
+// Greetr extends EventEmitter
+util.inherits(Greetr,EventEmitter);
 
-// the above this refers to {name:'Jane Doe'}
-obj.greet.call({name:'Jane Doe'}); // function greet有參數的話，逐一表列於後
+// 把function greet放到Greetr的prototype
+Greetr.prototype.greet = function(data){ // pass data to all the listeners
+    console.log(this.greeting+': '+data); // hello world
+    this.emit('greet',data); // 因為繼承，Greetr有拿到emit方法
+}
 
-obj.greet.apply({name:'Jane Doe2'}); // function greet有參數的話，全部寫在一個陣列裡面，放到第二個參數
+// 建立Greetr物件
+var greeter1 = new Greetr();
+// 把方法放進陣列
+greeter1.on('greet',function(data){
+    console.log('someone greeted: '+data);
+});
+
+// 建立Greetr物件
+var greeter2 = new Greetr();
+// 把方法放進陣列
+greeter2.on('greet',function(data){
+    console.log('greeter2 someone greeted: '+data);
+});
+
+// 從prototype拿到function greet
+greeter1.greet('Justin');
+// hello world (emit - event)
+// someone greeted (event listener)
+
+greeter2.greet('Hi there');

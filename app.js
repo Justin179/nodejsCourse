198107a54ed1592e9f02849a435c7932284a9c8d
Justin179 <Justin179>
2017-01-18 17:19:49 +0800

    use pipe 寫到檔案 & 寫到壓縮檔

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var zlib = require('zlib');
 
 // read from greet.txt
 var readable = fs.createReadStream(__dirname+'/greet.txt'
@@ -9,7 +10,18 @@ var readable = fs.createReadStream(__dirname+'/greet.txt'
 var writable = fs.createWriteStream(__dirname+'/greet2.txt');
 
 // when read a chunk of data from readable, invoke the function
-readable.on('data',function(chunk){
-    console.log(chunk);
-    writable.write(chunk); // 把資料寫(複製)到greet2.txt
-});
+// readable.on('data',function(chunk){
+//     console.log(chunk);
+//     writable.write(chunk); // 把資料寫(複製)到greet2.txt
+// });
+
+// 用pipe簡化寫法, 從file讀進來，(通過pipe)，寫出去到file
+
+var compressed = fs.createWriteStream(__dirname+'/greet.txt.gz');
+
+var gzip = zlib.createGzip();
+
+readable.pipe(writable);
+
+// on every chunk pipe to gzip, then piping the compressed data to a file
+readable.pipe(gzip).pipe(compressed);

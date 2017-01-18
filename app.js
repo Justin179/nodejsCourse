16899d76234642df97d7fb5ca9aa427a60ebe0c1
Justin179 <Justin179>
2017-01-18 15:19:51 +0800

    node Buffer

--- app.js
+++ app.js
@@ -1,46 +1,10 @@
-'use strict';
+// take a string and convert it to binary data
+// the second param is character encoding
+var buffer = new Buffer('hello','utf-8');
+console.log(buffer);
+console.log(buffer.toString());
+console.log(buffer.toJSON());
+console.log(buffer[1]);
 
-// 以es6 改寫eventEmitter
-
-// javascript實現了import 其他類別
-var Greetr = require('./greetr.js');
-
-// 以下一整塊搬到module
-// -----------------------------------------
-// var EventEmitter = require('events');
-
-// class Greetr extends EventEmitter {
-//     constructor(){
-//         super(); // 等同呼叫父類別的建構子EventEmitter.call(this);
-//         this.greeting = "Hello world";
-//     }
-
-//     greet(data){
-//         // console.log(this.greeting+': '+data); // hello world
-//         console.log(`${this.greeting}: ${data}`);
-//         this.emit('greet',data); // 因為繼承，Greetr有拿到emit方法
-//     }
-// }
-// -----------------------------------------
-
-
-// 建立Greetr物件
-var greeter1 = new Greetr();
-// 把方法放進陣列
-greeter1.on('greet',function(data){
-    console.log('someone greeted: '+data);
-});
-
-// 建立Greetr物件
-var greeter2 = new Greetr();
-// 把方法放進陣列
-greeter2.on('greet',function(data){
-    console.log('greeter2 someone greeted: '+data);
-});
-
-// 從prototype拿到function greet
-greeter1.greet('Justin');
-// hello world (emit - event)
-// someone greeted (event listener)
-
-greeter2.greet('Hi there');
+buffer.write('xx');
+console.log(buffer.toString());

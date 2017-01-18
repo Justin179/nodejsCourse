0a7a9000c7d933f6cc0e6f4d5da70e88aa464f91
Justin179 <Justin179>
2017-01-18 16:37:12 +0800

    read text data from a txt file

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 var fs = require('fs');
-// 這裡Sync指的是，process會等到資料都load完了，程式才會往下執行
-// 這不適用多人同時使用的平台，特別是檔案大時
-var greet = fs.readFileSync(__dirname+'/greet.txt','utf-8'); // './greet.txt'也可以
+var readable = fs.createReadStream(__dirname+'/greet.txt',{encoding:'utf-8'});
 
-console.log(greet);
-
-// 非同步
-var greet = fs.readFile(__dirname+'/greet.txt','utf-8',
-    function(err,data){
-        console.log(data);
-    });
-
-console.log('DONE'); // 非同步的證明 
+readable.on('data',function(chunk){
+    console.log(chunk);
+});

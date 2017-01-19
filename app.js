b9b55e525c2b0d89acef6cb23990b8e7f6010d07
Justin179 <Justin179>
2017-01-19 10:44:06 +0800

    Routing

--- app.js
+++ app.js
@@ -5,16 +5,28 @@ var fs = require('fs');
 
 http.createServer(function(req,res){
 
-    // 組response
-    res.writeHead(200,{'Content-Type':'application/json'});
+    // http request (totally ignored, not even looking at the incoming url)
 
-    var obj = {
-        firstname: 'Justin',
-        lastname: 'Chen'
+    // Routing
+    if(req.url==='/'){
+        // 讀進來(stream)，寫出去 一行解決
+        fs.createReadStream(__dirname+'/index.html').pipe(res);
+    } else if(req.url==='/api'){
+        // http response
+        // 組response
+        res.writeHead(200,{'Content-Type':'application/json'});
+        var obj = {
+            firstname: 'Justin',
+            lastname: 'Chen'
+        };
+        // string will be formatted as JSON format 
+        res.end(JSON.stringify(obj));
+    } else {
+        res.writeHead(404);
+        res.end();
     }
 
-    // string will be formatted as JSON format
-    res.end(JSON.stringify(obj));
+    
 
 }).listen(1337,'127.0.0.1');
 // localhost:1337 這個program就會收到(server object.listen())
